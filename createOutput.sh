053e15d0a20af7a294266e101aaecb3f5c0b99aa
Jared Sturdy <sturdy@cern.ch>
2014-01-06 15:31:25 +0100

    initial commit

---
+++ createOutput.sh
@@ -0,0 +1,4 @@
+hadd -f MultiJet_prediction.root 2013-11-10_V?/MultiJet*.root
+hadd -f MET_prediction.root 2013-11-10_V?/MET*.root
+hadd -f HTMHT_prediction.root 2013-11-10_V?/HTMHT*.root
+hadd -f SingleMu_prediction.root 2013-11-10_V?/SingleMu*.root

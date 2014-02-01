8de435712195ac5237c4703a8871624653710469
Jared Sturdy <sturdy@cern.ch>
2014-02-01 17:19:01 +0100

    second version of the submission script to go with the second version of smallAnalyzer.py

--- submission.py
+++ submission.py
@@ -1,26 +1,48 @@
 import sys,os
-#bounds = [1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
-bounds = [(x)*2+1 for x in range(50)]
-print bounds
-for i in range(len(bounds)-1):
+from files import noiseFiles
+#jobName = "singlemu"
+#jobName = "hpdnoise"
+#jobName = "met"
+jobName = "htmht"
+#jobName = "multijet"
 
-    cmd = "python smallAnalyzer.py -o %dto%d.root -l %d -u %d > & %dto%d.out"%(bounds[i],bounds[i+1]-1,
-                                                                               bounds[i],bounds[i+1],
-                                                                               bounds[i],bounds[i+1]-1
-                                                                               )
-    jobFile = open("./job%d.csh"%(i+1),'w')
+filesPerJob = 3
+fileList = noiseFiles[jobName]["files"]
+totalFiles = len(fileList)
+numJobs = totalFiles/filesPerJob
+moduloIs = totalFiles%filesPerJob
+if moduloIs > 0:
+    numJobs = numJobs + 1
+print jobName,totalFiles,filesPerJob,numJobs,moduloIs
+for i in range(numJobs):
+    jobNum = i + 1
+    filesList = ""
+    for j in range(filesPerJob):
+        if j == 0:
+            filesList = "%s"%(fileList[i*filesPerJob+j])
+        elif i*filesPerJob+j<totalFiles:
+            filesList = "%s,%s"%(filesList,fileList[i*filesPerJob+j])
+    print filesList
+    cmd = "python smallAnalyzer.py -o hpdInformation_job%d -j %s -f %s > & %s_job%d.out"%(jobNum,
+                                                                                          noiseFiles[jobName]["dir"],
+                                                                                          filesList,
+                                                                                          jobName,jobNum)
+    print cmd
+    jobFile = open("%s_job%d.csh"%(jobName,jobNum),'w')
     jobFile.write("""#!/bin/csh
-cd /afs/cern.ch/work/s/sturdy/public/CMSSW_6_2_0_patch1/src
+cd /afs/cern.ch/work/s/sturdy/public/CMSSW_6_2_0_patch1/src/
 cmsenv
-%s"""%(cmd))
-    cmd = "chmod 744 job%d.csh"%(i+1)
+%s\n"""%(cmd))
+    jobFile.close()
+
+    cmd = "chmod 755 %s_job%d.csh"%(jobName,jobNum)
     print cmd
     os.system(cmd)
 
-    cmd = "bsub -q 1nh -J job%d < job%d.csh"%(i+1,i+1)
+    cmd = "bsub -q 8nh -J %s_job%d < %s_job%d.csh"%(jobName,jobNum,jobName,jobNum)
     print cmd
-    #os.system(cmd)
+    os.system(cmd)
 
-##python smallAnalyzer.py -o secondGroup.root -l 10 -u 20 > & 10to19.out &
-##python smallAnalyzer.py -o thirdGroup.root -l 20 -u 30 > & 20to29.out &
-##python smallAnalyzer.py -o tenthGroup.root -l 90 -u 100 > & 90to99.out &
+##python smallAnalyzer_v2.py -o secondGroup.root -l 10 -u 20 > & 10to19.out &
+##python smallAnalyzer_v2.py -o thirdGroup.root -l 20 -u 30 > & 20to29.out &
+##python smallAnalyzer_v2.py -o tenthGroup.root -l 90 -u 100 > & 90to99.out &
